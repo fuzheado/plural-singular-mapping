80f397bd5052902d9247cdb3cba72487bc36c184
Andrew Lih <andrew.lih@gmail.com>
2019-01-04 08:15:14 -0500

    convert plural terms to singular

---
+++ convert-plurals-to-singular.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: latin-1 -*-
+
+# Script for converting capitalized and plural art vocabulary terms to lowercae singular, 
+# more appropriate for OpenRefine reconciliation to Wikidata
+
+# Use pattern package - https://www.clips.uantwerpen.be/pages/pattern-en
+from pattern.text.en import singularize
+
+with open('list.txt', 'rb') as textfile:
+    plurals = [line.strip() for line in textfile.readlines()]
+
+singles = [singularize(plural.lower()) for plural in plurals]
+
+# print singles
+print("\n".join(singles))
